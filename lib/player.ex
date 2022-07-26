38ac684e8a217965bc17cd6672a1e71731b9957e
Ian Nalyanya <42600121+i1d9@users.noreply.github.com>
2022-07-26 11:28:10 +0300

    Chapter 2 completed

--- lib/player.ex
+++ lib/player.ex
@@ -22,4 +22,19 @@ defmodule IslandsEngine.Player do
   def set_name(player, name) do
     Agent.update(player, fn state -> Map.put(state, :name, name) end)
   end
+
+
+  def to_string(player) do
+    "%Player{"<> string_body(player) <>"}"
+  end
+
+  defp string_body(player) do
+    state = Agent.get(player, &(&1))
+    ":name => " <> name_to_string(state.name) <> ", \n" <>
+    ":island_set => " <> IslandSet.to_string(state.island_set) <> ",\n" <>
+    ":board => " <> Board.to_string(state.board)
+  end
+
+  defp name_to_string(:none), do: ":none"
+  defp name_to_string(name), do: ~s("#{name}")
 end
