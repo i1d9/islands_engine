87243fd0898ab230d190d5d0ca3a3eb5c551c081
Ian Nalyanya <42600121+i1d9@users.noreply.github.com>
2022-07-28 09:27:04 +0300

    Web Interface

---
+++ lib/islands_interface_web/controllers/page_controller.ex
@@ -0,0 +1,16 @@
+defmodule IslandsInterfaceWeb.PageController do
+  use IslandsInterfaceWeb, :controller
+
+  def index(conn, _params) do
+    render(conn, "index.html")
+  end
+
+
+  def test(conn ,%{"name" => name})do
+    {:ok, _pid} = IslandsEngine.Game.start_link(name)
+
+    conn
+    |> put_flash(:info, "You entered the name: "<> name)
+    |> render("index.html")
+  end
+end
